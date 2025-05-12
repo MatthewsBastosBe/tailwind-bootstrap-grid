01e797cbd66b01517dd5896d3f1015bcc6c7fb72
MatthewsBastosBe <matthews.bastos@betalent.tech>
2025-05-12 10:44:34 -0300

    fix: update package.json main entry and tsup config format order for consistency

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
   entry: ['lib/index.ts'],
-  format: ['esm', 'cjs'],
+  format: ['cjs', 'esm'],
   dts: true,
   clean: true,
   outDir: 'build',
