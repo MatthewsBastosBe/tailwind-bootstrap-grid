53747d82de19f5e718faee5bddefe3f93840e102
MatthewsBastosBe <matthews.bastos@betalent.tech>
2025-05-12 11:57:56 -0300

    fix: update tsconfig and tsup config for improved type declaration handling

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,9 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   entry: ['lib/index.ts'],
   format: ['cjs', 'esm'],
-  dts: true,
+  dts: {
+    entry: './types/index.ts',
+  },
   clean: true,
   outDir: 'build',
 });
